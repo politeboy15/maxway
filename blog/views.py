9c005e1a4b8239afbed38f792802551248dc5038
politeboy15 <muhammad14toxtasinov@gmail.com>
2025-02-17 08:30:26 +0500

    created template folder

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 5
+
