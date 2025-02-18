08d7cf5c7ed2c82afa9ef91cd74030cfbba5ecdc
politeboy15 <muhammad14toxtasinov@gamil.com>
2025-02-18 18:31:23 +0500

    Added comments under the posts

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,19 @@ class Post(models.Model):
                                                 self.publish.month,
                                                 self.publish.day,
                                                 self.slug])
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email=models.EmailField()
+    body=models.TextField(max_length=2000)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    active=models.BooleanField(default=True)
+
+    class Meta:
+        ordering=('created',)
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}."
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from .models import Post
+from .forms import *
 from django.shortcuts import get_object_or_404,render
 from django.views.generic import ListView
 
@@ -6,10 +7,19 @@ from django.views.generic import ListView
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
-    paginate_by = 5
+    paginate_by = 3
     template_name = 'blog\post\list.html'
 
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status="published", publish__year=year, publish__month=month, publish__day=day)
+    
+    comments = post.comments.filter(active=True)
+    new_comment=None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data_request="POST")
+        if comment_form.is_valid():
+            new_comment=comment_form.save(commit=False)
+            new_comment.post=post
     return render(request, 'blog/post/details.html', {'post':post})
